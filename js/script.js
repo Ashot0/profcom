3fadb6c2c76916d4f60f9ec02d9b8ccbb7bb5e1e
Ashot0 <illia.holovan@gmail.com>
2023-06-05 20:33:25 +0300

    team_fix

--- js/script.js
+++ js/script.js
@@ -29,11 +29,15 @@ const isMobile = {
 
 const menu = document.getElementById('menu');
 const teamBoxes = document.getElementById('team__boxes')
-
+const teamBoxUp1 = document.getElementById('team__box__up1')
+const teamBoxUp2 = document.getElementById('team__box__up2')
 
 if (isMobile.any()) {
+	teamBoxUp1.classList.add('_up-team')
+	teamBoxUp2.classList.add('_up-team')
 	teamBoxes.classList.add('_short')
 	menu.classList.add('_hidden')
+
 }
 
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
